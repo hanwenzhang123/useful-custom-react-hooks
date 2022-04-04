98e74916c03a8dd752a8e322926af50004ed0d7e
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-04-03 22:06:34 -0400

    Update usePrevious.js

--- src/6-usePrevious/usePrevious.js
+++ src/6-usePrevious/usePrevious.js
@@ -1,13 +1,14 @@
+//store previous value
 import { useRef } from "react"
 
 export default function usePrevious(value) {
-  const currentRef = useRef(value)
+  const currentRef = useRef(value)  //set to the current value
   const previousRef = useRef()
 
-  if (currentRef.current !== value) {
-    previousRef.current = currentRef.current
-    currentRef.current = value
+  if (currentRef.current !== value) { //is the value we currently saved that is different than the value paased into usePrevious
+    previousRef.current = currentRef.current  //set the previous one to the current saved value
+    currentRef.current = value    //set the saved value to the actual current value
   }
 
-  return previousRef.current
+  return previousRef.current  //always return the prev value while we have saved it
 }
